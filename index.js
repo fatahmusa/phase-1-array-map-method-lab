5983f20fc0175333a3deb3e919b7f18ee54bdd08
Abdifatah Musa <lawznleytha@gmail.com>
2024-05-06 22:21:56 +0300

    complete

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(){
+  return tutorials.map(tutorial => {
+    const titleSplit = tutorial.split(' ')
+    const caseTitle = titleSplit.map(word => {
+    word.charAt(0).toUpperCase() + word.slice(1)
+    return(`${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+  })
+  return caseTitle.join(' ')
+});
+
 }
+
